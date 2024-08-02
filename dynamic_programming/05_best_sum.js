f73ed34c6b688fffba10acd72f15524fa640b07d
Md Shayon <mdshayon0@gmail.com>
2024-08-02 19:29:36 +0600

    Working with best sum

--- dynamic_programming/05_best_sum.js
+++ dynamic_programming/05_best_sum.js
@@ -1,3 +1,14 @@
 /**
  * 01:52:07 - https://youtu.be/oBt53YbR9Kk?t=6727
  */
+
+function bestSum(targetSum, numbers) {
+    
+}
+
+
+console.log("===== Brute Force =====");
+console.log(bestSum(7, [5, 3, 4, 7])); // Output: [7]
+console.log(bestSum(8, [2, 3, 5])); // Output: [3, 5]
+console.log(bestSum(8, [1, 4, 5])); // Output: [4, 4]
+console.log(bestSum(100, [1, 2, 5, 25])); // Output: [25, 25, 25, 25]
